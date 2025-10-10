be5bfa1210dd3594b90177c272fa6df7b468f967
Vengatesankh <khvengatesan@gmail.com>
2025-10-10 09:58:29 +0530

    backend Completed

--- backend/controllers/ownerController.js
+++ backend/controllers/ownerController.js
@@ -1,4 +1,5 @@
 import imageKit from "../config/imageKit.js";
+import Booking from "../model/Booking.js";
 import Car from "../model/Car.js";
 import User from "../model/User.js";
 import fs from "fs";
@@ -113,8 +114,56 @@ export const getDashboardData = async (req,res)=>{
         }
 
         const cars = await Car.find({owner:_id});
+        const bookings = await Booking.find({owner:_id}).populate('car').sort({createdAt:-1});
+
+        const pendingBookings = await Booking.find({owner:_id, status:"pending"})
+        const completedBookings = await Booking.find({owner:_id,status:"confirmed"});
+        
+        //calculate monthlyRevenue from booking where status is confirmed
+        const monthlyRevenue = bookings.slice().filter(booking => booking.status === 'confirmed').reduce((acc,booking)=>acc + booking.price, 0)
+
+        const dashboardData = {
+            totalCars:cars.length,
+            totalBookings:bookings.length,
+            pendingBookings:pendingBookings.length,
+            completedBookings:completedBookings.length,
+            recentBookings:bookings.slice(0,3),
+            monthlyRevenue
+        }
+        res.json({success:true,dashboardData})
     } catch (error) {
         console.log(error.message);
         res.json({success:false , message:error.message})
+    }
+}
+
+//api to update user image
+export const updateUserImage = async (req,res)=>{
+    try {
+        const {_id} = req.user;
+        const imageFile = req.file;
+
+        //upload image to imageKit
+        const fileBuffer = fs.readFileSync(imageFile.path)
+        const response =  await imageKit.upload({
+            file:fileBuffer,
+            fileName: imageFile.originalname,
+            folder: '/users'
+        })
+           //optimization through imagekit url transformation
+        var optimizedImageUrl = imageKit.url({
+            path:response.filePath,
+            transformation: [
+                {width: '400'}, //width resizing
+                {quality:'auto'}, //auto compression
+                {format: 'webp'} //convert to modern format
+            ]
+        })
+        const image = optimizedImageUrl;
+
+        await User.findByIdAndUpdate(_id,{image});
+        res.json({success:true, message:"image updated"})
+    } catch (error) {
+        
     }
 }
